677021610fe2f1d5eecbb8f96e7b9f0cbc72aae8
千葉工 <chibataku0815@gmail.com>
2017-11-15 22:12:52 +0900

    scaffold book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115125853) do
+ActiveRecord::Schema.define(version: 20171115130903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,17 @@ ActiveRecord::Schema.define(version: 20171115125853) do
   create_table "authors_books", id: false, force: :cascade do |t|
     t.bigint "author_id", null: false
     t.bigint "book_id", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "isbn"
+    t.string "title"
+    t.integer "price"
+    t.string "publish"
+    t.date "published"
+    t.boolean "dl"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
